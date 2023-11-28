333c1a76e66488a008343b71bae5c48f2e6e2d1e
Ganash <ganeshkv1995@gmail.com>
2023-11-28 11:05:49 +0530

    Created and Updated README.md, GreetingController and Greetingrecords

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Scaler-Spring-Demo-14Sep2023
 Scaler Spring Demo Class Homework on 14 Sep 2023
+
+Creating a simple Spring Web API to greet users.
+
+URLS to test:
+
+http://localhost:8080/greeting
+
+http://localhost:8080/greeting?name=Ganesh
---
+++ src/main/java/com/springdemo/scalerspringdemo/ScalerspringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.scalerspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerspringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalerspringdemoApplication.class, args);
+	}
+
+}
